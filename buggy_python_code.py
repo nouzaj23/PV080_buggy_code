34b1975e6e5fe06cc82f513651930aa4a9c01672
Jan Nouza <nouzaj23@gmail.com>
2024-04-17 10:39:06 +0200

    Fix spacing mistakes

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,5 +1,5 @@
-import sys 
-import os
+import urllib
+
 import yaml
 import flask
 
@@ -14,6 +14,8 @@ def index():
 
         
 CONFIG = {"API_KEY": "771df488714111d39138eb60df756e6b"}
+
+
 class Person(object):
     def __init__(self, name):
         self.name = name
@@ -37,13 +39,15 @@ def fetch_website(urllib_version, url):
 
 def load_yaml(filename):
     stream = open(filename)
-    deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
+    deserialized_data = yaml.load(stream, Loader=yaml.Loader)  # deserializing data
     return deserialized_data
-    
+
+
 def authenticate(password):
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
     print("Successfully authenticated!")
+
 
 if __name__ == '__main__':
     print("Vulnerabilities:")
@@ -64,4 +68,3 @@ if __name__ == '__main__':
     elif choice == "4":
         password = input("Enter master password: ")
         authenticate(password)
-
