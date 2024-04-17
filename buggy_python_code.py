491ccfa8d5476f803652c309dc5033bdc5772303
Jan Nouza <nouzaj23@gmail.com>
2024-04-17 10:53:02 +0200

    Fix some more mistakes

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,4 +1,4 @@
-import urllib
+import urllib3
 import yaml
 import flask
 
@@ -24,13 +24,10 @@ def print_nametag(format_string, person):
     print(format_string.format(person=person))
 
 
-def fetch_website(urllib_version, url):
-    # Import the requested version (2 or 3) of urllib
-    exec(f"import urllib{urllib_version} as urllib", globals())
+def fetch_website(url):
     # Fetch and print the requested URL
-
     try:
-        http = urllib.PoolManager()
+        http = urllib3.PoolManager()
         r = http.request('GET', url)
     except:
         print('Exception')
@@ -60,7 +57,7 @@ if __name__ == '__main__':
         print_nametag(input("Please format your nametag: "), new_person)
     elif choice == "2":
         urlib_version = input("Choose version of urllib: ")
-        fetch_website(urlib_version, url="https://www.google.com")
+        fetch_website(urlib_version)
     elif choice == "3":
         load_yaml(input("File name: "))
         print("Executed -ls on current folder")
